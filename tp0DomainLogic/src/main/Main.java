b3f482fe1bc3ff144031e930d26d85de826dfa35
dtmarcocci <marcoccitomas@gmail.com>
2023-03-11 22:27:30 -0300

    Se termino el tp 0

--- tp0DomainLogic/src/main/Main.java
+++ tp0DomainLogic/src/main/Main.java
@@ -16,11 +16,8 @@ public class Main {
 		///////// Explicacion /////////
 
 		/*
-		 * El Patron de diseño ( o anti patron) anemico consiste en que las clases del
-		 * dominio solo contengan los atributos que la componen y como metodos
-		 * getter/setters y constructores Sin tener logica de negocio en las mismas. Por
-		 * otro lado este anti patron maneja toda la logica de negocio desde la capa de
-		 * servicio
+		 * Aca la logica de como se transforma el objeto Tiempo es realizado en la capa
+		 * de dominio y la capa de servicio solo imprime el resultado
 		 */
 
 	}
